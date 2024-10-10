33b73f33d0c4e659b52b6a50d9817157ec6425c9
TimoDiepers <timo.diepers@gmail.com>
2024-10-10 15:27:44 +0200

    add location lookup in init

--- sentier_peakachu/__init__.py
+++ sentier_peakachu/__init__.py
@@ -4,9 +4,11 @@ __all__ = (
     "__version__",
     "ElectricityModel",
     "create_local_datastorage",
+    "get_geonames_iri_from_iso_code",
 )
 
 __version__ = "0.1.0"
 
 from sentier_peakachu.data import create_local_electricity_datastorage
 from sentier_peakachu.model import ElectricityModel
+from sentier_peakachu.utils_location import get_geonames_iri_from_iso_code
