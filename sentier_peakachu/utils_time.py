cbd9f95516ff5ebc04ad5cf75cacc51e548a6bb8
TimoDiepers <timo.diepers@gmail.com>
2024-10-11 11:31:29 +0200

    update start and end time iris in filter_timespan

--- sentier_peakachu/utils_time.py
+++ sentier_peakachu/utils_time.py
@@ -8,11 +8,11 @@ def filter_timespan(
 ) -> pd.DataFrame:
     return df[
         (
-            df["https://example.com/units/start_time"]
+            df["https://example.com/model-terms/start_time"]
             <= begin_date.strftime("%Y-%m-%d %H:%M:%S")
         )
         & (
-            df["https://example.com/units/end_time"]
+            df["https://example.com/model-terms/end_time"]
             >= end_date.strftime("%Y-%m-%d %H:%M:%S")
         )
     ]
